3ffc6c6350e26d64cb97f3fe9ea5ba6379fe3f0f
Ante Petkovic <ante.petkovic01@gmail.com>
2024-03-19 18:46:04 +0100

    hangman sketch

--- src/components/features/game/Game.tsx
+++ src/components/features/game/Game.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
 import { fetchQuote } from '@store/features/quote/quote-slice';
+import HangmanSketch from './components/HangmanSketch/HangmanSketch';
 
 function Game() {
   const { isLoading, quote } = useAppSelector((state) => state.quote);
@@ -12,7 +13,18 @@ function Game() {
 
   if (isLoading) return <div>loading</div>;
 
-  return <div>{quote.content}</div>;
+  return (
+    <div>
+      <div>{quote.content}</div>
+      <div
+        style={{
+          display: 'flex'
+        }}
+      >
+        <HangmanSketch numberOfMisses={6} />
+      </div>
+    </div>
+  );
 }
 
 export default Game;
---
+++ src/components/features/game/components/HangmanSketch/HangmanSketch.tsx
@@ -0,0 +1,43 @@
+const HEAD = (
+  <div className="w-[35px] h-[35px] border-[5px] border-slate-100 rounded-full absolute top-[50px] right-[-15px]" />
+);
+
+const BODY = (
+  <div className="w-[5px] h-[75px] bg-slate-100 absolute top-[85px] right-0" />
+);
+
+const RIGHT_ARM = (
+  <div className="w-[60px] h-[5px] bg-slate-100 absolute top-[130px] right-[-60px] transform rotate-[-30deg] origin-bottom-left" />
+);
+
+const LEFT_ARM = (
+  <div className="w-[60px] h-[5px] bg-slate-100 absolute top-[130px] right-[5px] transform rotate-[30deg] origin-bottom-right" />
+);
+
+const RIGHT_LEG = (
+  <div className="w-[60px] h-[5px] bg-slate-100 absolute top-[155px] right-[-55px] transform rotate-[60deg] origin-bottom-left" />
+);
+
+const LEFT_LEG = (
+  <div className="w-[60px] h-[5px] bg-slate-100 absolute top-[155px] right-0 transform rotate-[-60deg] origin-bottom-right" />
+);
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+interface HangmanSketchProps {
+  numberOfMisses: number;
+}
+
+function HangmanSketch({ numberOfMisses }: HangmanSketchProps) {
+  return (
+    <div className="relative">
+      {BODY_PARTS.slice(0, numberOfMisses)}
+      <div className="h-[50px] w-[5px] bg-slate-100 absolute top-0 right-0" />
+      <div className="h-[5px] w-[150px] bg-slate-100 ml-[50px]" />
+      <div className="h-[200px] w-[5px] bg-slate-100 ml-[50px]" />
+      <div className="h-[5px] w-[100px] bg-slate-100" />
+    </div>
+  );
+}
+
+export default HangmanSketch;
